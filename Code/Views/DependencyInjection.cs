715fdc808399d0dc91c1e9cd90b8d96668dd29b1
Xiphereal <lozanojalex@gmail.com>
2024-07-22 20:19:14 +0200

    feat: add Timer that ticks every second to the Models.Time

--- Code/Views/DependencyInjection.cs
+++ Code/Views/DependencyInjection.cs
@@ -3,14 +3,23 @@ using Models.Characters;
 
 public partial class DependencyInjection : Control
 {
+    private Models.Time time;
+
     public override void _Ready()
     {
         Warrior warrior = Character.Warrior;
-
         // lvl 2, with Charge.
         warrior.LevelUp();
 
+        time = new Models.Time();
+        warrior.AffectedBy(time);
+
         GetNode<CharacterFrame>("YourCharacterFrame").Warrior = warrior;
         GetNode<ActionBar>("ActionBar").Warrior = warrior;
+    }
+
+    public void TimeTicked()
+    {
+        time.Pass();
     }
 }
